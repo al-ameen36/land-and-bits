f08dc878cfcc299a1a42a7d4bb6607e734161cc9
al-ameen <muhdabdullahi36@gmail.com>
2025-02-12 02:44:03 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+import Map from "./pages/map";
+
+function App() {
+  return (
+    <>
+      <Map />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/map.tsx
@@ -0,0 +1,5 @@
+import styles from "./map.module.scss";
+
+export default function Map() {
+  return <div id="map-container" className={styles.map} />;
+}
